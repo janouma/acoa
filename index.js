78e972c3e3f6216e77018838517a80fb0bcda815
Judicaël Anouma <janouma@gmail.com>
2020-09-06 12:21:43 +0200

    remove all but logo

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = {
-  ...require('./lib/collection_factory'),
-  bundleTransactionAction: require('./lib/transaction_action_bundler'),
-  setupGraph: require('./lib/graph_setup')
-}
--- test/connector.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { Database } = require('arangojs')
-
-const connector = new Database({ url: process.env.npm_package_config_testDbUrl })
-connector.useDatabase(process.env.npm_package_config_testDbName)
-
-module.exports = connector
